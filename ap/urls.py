13813de6e50835f08ea83db5156d0bf6059a392e
Amish2101 <kaklotaramish@gmail.com>
2022-04-29 19:01:16 +0530

    Set login form

--- ap/urls.py
+++ ap/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from .views import Home, Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('', home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ap/views.py
+++ ap/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class home(TemplateView):
-    template_name = 'base.html'
+class Home(TemplateView):
+    template_name = 'base/base.html'
+
+class Login(TemplateView):
+    template_name = 'base/signin.html'
